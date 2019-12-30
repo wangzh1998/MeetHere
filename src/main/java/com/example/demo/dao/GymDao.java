db70632ed1f1d010e7b5b2b2de320b21141d8bf7
wangzh <1759630417@qq.com>
2019-12-30 17:32:29 +0800

    # 修改GymDao查询可用场馆的SQL，解决集成测试SQL语法错误的问题

--- src/main/java/com/example/demo/dao/GymDao.java
+++ src/main/java/com/example/demo/dao/GymDao.java
@@ -31,7 +31,17 @@ public interface GymDao extends JpaRepository<Gym,Integer> {
     /*
     可用场馆的查询只排除被课程占据的，如果是部分场地被预约，则不考虑
     * */
-    @Query(value = "select gym.gym_id as gymId,gym.gym_name as gymName,gym.start_time as startTime,gym.end_time as endTime\n" +
+  /*  @Query(value = "select gym.gym_id as gymId,gym.gym_name as gymName,gym.start_time as startTime,gym.end_time as endTime\n" +
+            "from gym\n" +
+            "where start_time <= :startTime and end_time >= :endTime " +
+            "and  gym_id \n" +
+            "not in \n" +
+            "(select gym_id \n" +
+            "from course\n" +
+            "where course.weekday = :weekday and course.end_time > :startTime and course.start_time < :endTime)\n",
+            nativeQuery = true)*/
+  //删除别名
+    @Query(value = "select gym.gym_id,gym.gym_name,gym.start_time,gym.end_time\n" +
             "from gym\n" +
             "where start_time <= :startTime and end_time >= :endTime " +
             "and  gym_id \n" +
